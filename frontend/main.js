69eedbac873b6c4d953ea4235273360fb3bcd5ae
Antonio Vilchez <antoniovmonge@gmail.com>
2021-11-28 17:02:12 +0100

    frontend basic working

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,19 @@
+let loginBtn = document.getElementById('login-btn')
+let logoutBtn = document.getElementById('logout-btn')
+
+let token = localStorage.getItem('token')
+
+if (token) {
+    loginBtn.remove()
+} else {
+    logoutBtn.remove()
+}
+
+logoutBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    localStorage.removeItem('token')
+    window.location = 'file://wsl%24/Ubuntu/home/antonio/code/antoniovmonge/djangoivy/frontend/login.html'
+})
 
 let projectsUrl = 'http://127.0.0.1:8000/api/projects/'
 
@@ -13,6 +29,7 @@ let getProjects = () => {
 
 let buildProjects = (projects) => {
     let projectsWrapper = document.getElementById('projects--wrapper')
+    projectsWrapper.innerHTML = ''
 
     for (let i =0; projects.length > i; i++) {
         let project = projects[i]
@@ -23,8 +40,10 @@ let buildProjects = (projects) => {
                 <div>
                     <div class="card--header">
                         <h3>${project.title}</h3>
-                        <strong class="vote--option">&#43;</strong>
-                        <strong class="vote--option">&#8722;</strong>
+                        <strong class="vote--option" data-vote="up" 
+                            data-project="${project.id}" >&#43;</strong>
+                        <strong class="vote--option" data-vote="down"
+                            data-project="${project.id}" >&#8722;</strong>
                     
                     </div>
                     <i>${project.vote_ratio}% Positive Feedback </i>
@@ -34,6 +53,37 @@ let buildProjects = (projects) => {
         `
 
         projectsWrapper.innerHTML += projectCard
+    }
+
+    addVoteEvents()
+
+    //Add a listener
+}
+
+let addVoteEvents = () => {
+    let voteBtns = document.getElementsByClassName("vote--option")
+    
+    for (let i =0; voteBtns.length > i; i++){
+        voteBtns[i].addEventListener('click', (e) => {
+            let token = localStorage.getItem('token')
+            console.log('TOKEN:', token)
+            let vote = e.target.dataset.vote
+            let project = e.target.dataset.project
+            
+            fetch(`http://127.0.0.1:8000/api/projects/${project}/vote/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                },
+                body: JSON.stringify({ 'value': vote })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data)
+                    getProjects()
+                })
+        })
     }
 }
 
